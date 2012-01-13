b08f08996b6697949cd80a9f8c4923ba708ff5ff
Pontus Lurcock <pont@talvi.net>
2012-01-14 10:58:25 +1300

    Closed #177 (host PuffinPlot publicly). PuffinPlot is now hosted on Google Code.

---
+++ manual/make-all.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+for ext in aux log 4ct 4tc bbl blg tmp xref out idv lg css dvi html pdf; do
+  rm manual.$ext;
+done
+hg tip --template '\\def\\HgNode{{node|short}}\n\\def\\HgDate{{date|isodate}}\n\\def\\HgAuthor{{author|person}}\n' >hg.id
+pdflatex manual
+bibtex manual
+pdflatex manual
+pdflatex manual
+htlatex manual
+bibtex manual
+htlatex manual
+htlatex manual
