47dec66a1c2bac76548b01802e346230d6dec4be
Pontus Lurcock <pont@talvi.net>
2012-01-25 18:45:07 +1300

    Final tweaks for 0.95 release. A couple more corrections in the manual, and minor changes to the build file (executable jar file and correct upload path).

--- src/net/talvi/puffinplot/data/Suite.java
+++ src/net/talvi/puffinplot/data/Suite.java
@@ -314,7 +314,7 @@ public final class Suite {
     /**
      * <p>Creates a new suite from the specified files.
      * The is a convenience method for
-     * {@link Suite(List<File>, SensorLengths, TwoGeeLoader.Protocol)}
+     * {@link #Suite(List, SensorLengths, TwoGeeLoader.Protocol)}
      * using the default sensor lengths (1, 1, 1) and protocol 
      * ({@code NORMAL}).
      * </p>
