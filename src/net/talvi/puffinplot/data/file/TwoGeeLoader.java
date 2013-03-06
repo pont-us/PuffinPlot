9c887cfbf49b99e5d6aa83eac409b2b12db6c683
Pontus Lurcock <pont@talvi.net>
2013-03-06 12:02:19 +0100

    Opened, fixed, and closed bug #267 (discrete/continuous heuristic) #267 PuffinPlot does not read file 516_1.DAT correctly

--- src/net/talvi/puffinplot/data/file/TwoGeeLoader.java
+++ src/net/talvi/puffinplot/data/file/TwoGeeLoader.java
@@ -389,7 +389,7 @@ public class TwoGeeLoader extends AbstractFileLoader {
             measType = measTypeFromString(r.getString("Meas. type", "sample/continuous"));
         } else if (fieldExists("Depth")) {
             measType = MeasType.CONTINUOUS;
-        } else if (fieldExists("Position") && !fieldExists("Depth")) {
+        } else if (fieldExists("Position") && !fieldExists("Sample ID")) {
             measType = MeasType.CONTINUOUS;
         } else {
             measType = MeasType.DISCRETE;
