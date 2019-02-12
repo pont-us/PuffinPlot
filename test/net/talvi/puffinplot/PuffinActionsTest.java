d3ba2f93d293b11d4359706f55424af784f8ef9b
Pontus Lurcock <pont@talvi.net>
2019-02-12 11:15:30 +0100

    Add more unit tests for PuffinActions

--- test/net/talvi/puffinplot/PuffinActionsTest.java
+++ test/net/talvi/puffinplot/PuffinActionsTest.java
@@ -16,7 +16,13 @@
  */
 package net.talvi.puffinplot;
 
+import java.util.function.Consumer;
+import net.talvi.puffinplot.data.AmsCalculationType;
+import net.talvi.puffinplot.data.MeasurementAxis;
+import net.talvi.puffinplot.data.Sample;
+import net.talvi.puffinplot.data.Suite;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class PuffinActionsTest {
@@ -61,9 +67,231 @@ public class PuffinActionsTest {
     }
     
     @Test
+    public void testSave() {
+        actions.save.actionPerformed(null);
+        Mockito.verify(app).save();
+    }
+    
+    @Test
+    public void testSaveAsNoSuite() {
+        actions.saveAs.actionPerformed(null);
+        Mockito.verify(app, Mockito.never()).showSaveAsDialog(Mockito.any());
+    }
+    
+    @Test
+    public void testSaveAsWithSuite() {
+        final Suite suite = new Suite("test");
+        Mockito.when(app.getSuite()).thenReturn(suite);
+        actions.saveAs.actionPerformed(null);
+        Mockito.verify(app).showSaveAsDialog(Mockito.any());
+    }
+    
+    @Test
     public void testPageSetup() {
         actions.pageSetup.actionPerformed(null);
         Mockito.verify(app).showPageSetupDialog();
+    }
+    
+    @Test
+    public void testFlipSampleX() {
+        actions.flipSampleX.actionPerformed(null);
+        Mockito.verify(app).flipSelectedSamples(MeasurementAxis.X);
+    }
+    
+    @Test
+    public void testFlipSampleY() {
+        actions.flipSampleY.actionPerformed(null);
+        Mockito.verify(app).flipSelectedSamples(MeasurementAxis.Y);
+    }
+    
+    @Test
+    public void testFlipSampleZ() {
+        actions.flipSampleZ.actionPerformed(null);
+        Mockito.verify(app).flipSelectedSamples(MeasurementAxis.Z);
+    }
+    
+    @Test
+    public void testInvertSamples() {
+        actions.invertSamples.actionPerformed(null);
+        Mockito.verify(app).invertSelectedSamples();
+    }
+    
+    @Test
+    public void pcaOnSelection() {
+        actions.pcaOnSelection.actionPerformed(null);
+        Mockito.verify(app).doPcaOnSelection();
+    }
+    
+    @Test
+    public void testCircleFit() {
+        actions.circleFit.actionPerformed(null);
+        Mockito.verify(app).fitGreatCirclesToSelection();
+    }
+    
+    @Test
+    public void testGreatCircleAnalysis() {
+        actions.greatCircleAnalysis.actionPerformed(null);
+        Mockito.verify(app).calculateGreatCirclesDirections();
+    }
+    
+    @Test
+    public void testClearSiteCalcs() {
+        actions.clearSiteCalcs.actionPerformed(null);
+        Mockito.verify(app).clearSiteCalculations();
+    }
+    
+    @Test
+    public void testClearSamplePca() {
+        actions.clearSamplePca.actionPerformed(null);
+        final Sample sample = modifySelectedSamplesHelper();
+        Mockito.verify(sample).clearPca();
+        Mockito.verifyNoMoreInteractions(sample);
+    }
+    
+    @Test
+    public void testClearSampleGreatCircle() {
+        actions.clearSampleGreatCircle.actionPerformed(null);
+        final Sample sample = modifySelectedSamplesHelper();
+        Mockito.verify(sample).clearGreatCircle();
+        Mockito.verifyNoMoreInteractions(sample);
+    }
+
+    @Test
+    public void testClearSampleCalcs() {
+        actions.clearSampleCalcs.actionPerformed(null);
+        final Sample sample = modifySelectedSamplesHelper();
+        Mockito.verify(sample).clearCalculations();
+        Mockito.verifyNoMoreInteractions(sample);
+    }
+    
+    /**
+     * We can't directly compare method references (two different instances
+     * of "Sample::clearPca" will give two different lambdas) so this is
+     * a workaround to check, imperfectly, that the correct sample consumer
+     * method reference is passed to PuffinApp#modifySelectedSamples:
+     * the sample-consumer argument to modifySelectedSamples is captured
+     * and invoked on a mocked sample. The mocked sample is passed back to
+     * the caller, who can verify the interaction on it.
+     * 
+     * @return 
+     */
+    private Sample modifySelectedSamplesHelper() {
+        ArgumentCaptor<Consumer<Sample>> captor =
+                ArgumentCaptor.forClass(Consumer.class);
+        Mockito.verify(app).modifySelectedSamples(captor.capture());
+        final Sample sample = Mockito.mock(Sample.class);
+        final Consumer<Sample> consumer = captor.getValue();
+        consumer.accept(sample);
+        return sample;
+    }
+    
+    @Test
+    public void testCopyStepSelection() {
+        actions.copyStepSelection.actionPerformed(null);
+        Mockito.verify(app).copyStepSelection();
+    }
+    
+    @Test
+    public void testPasteStepSelection() {
+        actions.pasteStepSelection.actionPerformed(null);
+        Mockito.verify(app).pasteStepSelection();
+    }
+    
+    @Test
+    public void testPrefs() {
+        actions.prefs.actionPerformed(null);
+        Mockito.verify(app).showPreferences();
+    }
+    
+    @Test
+    public void testPrint() {
+        actions.print.actionPerformed(null);
+        Mockito.verify(app).showPrintDialog("MAIN");
+    }
+    
+    @Test
+    public void testPrintSiteEqualArea() {
+        actions.printSiteEqualArea.actionPerformed(null);
+        Mockito.verify(app).showPrintDialog("SITE");
+    }
+    
+    @Test
+    public void testPrintSuiteEqualArea() {
+        actions.printSuiteEqualArea.actionPerformed(null);
+        Mockito.verify(app).showPrintDialog("SUITE");
+    }
+    
+    @Test
+    public void testQuit() {
+        actions.quit.actionPerformed(null);
+        Mockito.verify(app).quit();
+    }
+    
+    @Test
+    public void testImportAms() {
+        actions.importAms.actionPerformed(null);
+        Mockito.verify(app).showImportAmsDialog();
+    }
+    
+    @Test
+    public void testMultiSuiteMeans() {
+        actions.multiSuiteMeans.actionPerformed(null);
+        Mockito.verify(app).calculateMultiSuiteMeans();
+    }
+    
+    @Test
+    public void testShowCustomFlagsWindow() {
+        actions.showCustomFlagsWindow.actionPerformed(null);
+        Mockito.verify(app).showCustomFlagsWindow();
+    }
+    
+    @Test
+    public void testShowCustomNotesWindow() {
+        actions.showCustomNotesWindow.actionPerformed(null);
+        Mockito.verify(app).showCustomNotesWindow();
+    }
+    
+    @Test
+    public void testBootAmsNaive() {
+        actions.bootAmsNaive.actionPerformed(null);
+        Mockito.verify(app).doAmsCalc(AmsCalculationType.BOOT, "bootams.py");
+    }
+    
+    @Test
+    public void testBootAmsParam() {
+        actions.bootAmsParam.actionPerformed(null);
+        Mockito.verify(app).
+                doAmsCalc(AmsCalculationType.PARA_BOOT, "bootams.py");
+    }
+    
+    @Test
+    public void testHestAms() {
+        actions.hextAms.actionPerformed(null);
+        Mockito.verify(app).doAmsCalc(AmsCalculationType.HEXT, "s_hext.py");
+    }
+    
+    @Test
+    public void testRescaleMagSus() {
+        actions.rescaleMagSus.actionPerformed(null);
+        Mockito.verify(app).showRescaleMagSusDialog();
+    }
+    
+    @Test
+    public void clearPreferences() {
+        actions.clearPreferences.actionPerformed(null);
+        Mockito.verify(app).clearPreferences();
+    }
+    
+    @Test
+    public void testImportPrefs() {
+        actions.importPrefs.actionPerformed(null);
+        Mockito.verify(app).showImportPreferencesDialog();
+    }
+    
+    @Test
+    public void testClearAmsCalcs() {
+        actions.clearAmsCalcs.actionPerformed(null);
+        Mockito.verify(app).clearAmsCalcs();
     }
     
     @Test
