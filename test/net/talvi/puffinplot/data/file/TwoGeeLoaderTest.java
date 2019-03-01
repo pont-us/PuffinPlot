4d3ae129cf348f2abd37bdf04b7dff66399f44d5
Pontus Lurcock <pont@talvi.net>
2019-03-01 19:55:26 +0100

    Add another test for TwoGeeReader

--- test/net/talvi/puffinplot/data/file/TwoGeeLoaderTest.java
+++ test/net/talvi/puffinplot/data/file/TwoGeeLoaderTest.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import net.talvi.puffinplot.TestUtils;
+import net.talvi.puffinplot.data.ArmAxis;
 import net.talvi.puffinplot.data.MeasurementType;
 import net.talvi.puffinplot.data.TreatmentStep;
 import net.talvi.puffinplot.data.TreatmentType;
@@ -39,21 +40,6 @@ import org.junit.rules.TemporaryFolder;
 public class TwoGeeLoaderTest {
 
     
-    private static final double[][] expected1 = {
-        // dec    int    intensity   Run #  TT AF X  AF Y  AF Z
-        {247.76,  66.28, 1.1881E-05, 17208, 0,    0,    0,    0},
-        {281.47,  71.52, 9.0456E-06, 17209, 1,   50,   50,   50},
-        {276.52,  72.24, 6.2841E-06, 17210, 1,  100,  100,  100},
-        {271.92,  65.34, 4.3688E-06, 17213, 1,  150,  150,  150},
-        {265.57,  62.32, 4.1486E-06, 17214, 1,  200,  200,  200},
-        {269.25,  72.97, 2.9370E-06, 17215, 1,  250,  250,  250},
-        {213.53,  69.43, 2.6551E-06, 17216, 1,  300,  300,  300},
-        {207.01,  49.40, 1.6676E-06, 17217, 1,  400,  400,  400},
-        { 17.13,  72.37, 7.5306E-07, 17218, 1,  500,  500,  500},
-        {128.47, -36.61, 5.9503E-07, 17219, 1,  600,  600,  600},
-        {166.37, -25.55, 3.0570E-07, 17220, 1,  800,  800,  800},
-        { 12.15, -27.18, 8.9747E-07, 17221, 1, 1000, 1000, 1000}
-    };
     private static final double DELTA = 1e-10;
     
     @Rule
@@ -90,27 +76,38 @@ public class TwoGeeLoaderTest {
         assertTrue(loader.getMessages().isEmpty());
     }
 
+    private static final double[][] expected_sg12_7 = {
+        // dec    int    intensity   Run #  TT AF X  AF Y  AF Z
+        {247.76, 66.28, 1.1881E-05, 17208, 0, 0, 0, 0},
+        {281.47, 71.52, 9.0456E-06, 17209, 1, 50, 50, 50},
+        {276.52, 72.24, 6.2841E-06, 17210, 1, 100, 100, 100},
+        {271.92, 65.34, 4.3688E-06, 17213, 1, 150, 150, 150},
+        {265.57, 62.32, 4.1486E-06, 17214, 1, 200, 200, 200},
+        {269.25, 72.97, 2.9370E-06, 17215, 1, 250, 250, 250},
+        {213.53, 69.43, 2.6551E-06, 17216, 1, 300, 300, 300},
+        {207.01, 49.40, 1.6676E-06, 17217, 1, 400, 400, 400},
+        {17.13, 72.37, 7.5306E-07, 17218, 1, 500, 500, 500},
+        {128.47, -36.61, 5.9503E-07, 17219, 1, 600, 600, 600},
+        {166.37, -25.55, 3.0570E-07, 17220, 1, 800, 800, 800},
+        {12.15, -27.18, 8.9747E-07, 17221, 1, 1000, 1000, 1000}
+    };
+    
     @Test
     public void testWithSg12_7() throws IOException {
-        final String filename = "SG12-7.DAT";
-        final Path filePath
-                = temporaryFolder.getRoot().toPath().resolve(filename);
-        Files.copy(
-                TestFileLocator.class.getResourceAsStream("twogee/" + filename),
-                filePath);
+        final Path filePath = copyFile("SG12-7.DAT");
         for (boolean polar: new boolean[] {false, true}) {
             final TwoGeeLoader loader = new TwoGeeLoader(filePath.toFile(),
                     TwoGeeLoader.Protocol.NORMAL, new Vec3(1, 1, 1), polar);
             for (int i = 0; i < loader.getTreatmentSteps().size(); i++) {
-                final double[] expected = expected1[i];
+                final double[] expected = expected_sg12_7[i];
                 final TreatmentStep step = loader.getTreatmentSteps().get(i);
-                checkVariableFields(expected, step);
-                checkConstantFields(step);
+                checkVariableFieldsSg12_7(expected, step);
+                checkConstantFieldsSg12_7(step);
             }
         }
     }
     
-    private static void checkConstantFields(TreatmentStep step) {
+    private static void checkConstantFieldsSg12_7(TreatmentStep step) {
         assertEquals(8.02, step.getVolume(), DELTA);
         /*
          * TwoGeeLoader doesn't read the depth for discrete samples; the slot
@@ -128,27 +125,27 @@ public class TwoGeeLoaderTest {
         assertTrue(Double.isNaN(step.getIrmField()));
         assertEquals(MeasurementType.DISCRETE, step.getMeasurementType());
     }
-    
-    private static void checkVariableFields(double[] expected,
+
+    private static void checkVariableFieldsSg12_7(double[] expected,
             TreatmentStep step) {
         assertEquals(expected[0], step.getMoment().getDecDeg(), 0.005);
         assertEquals(expected[1], step.getMoment().getIncDeg(), 0.005);
         assertEquals(expected[2] * 1000, step.getMoment().mag(), 5e-5);
-        
+
         /*
          * Due to a bug in the 2G software, (position - 1) is added to the run
          * number stored in the 2G file. The "expected" value in the array is
          * taken directly for the 2G file, so we check the loaded run number not
          * against the original 2G value but against a corrected value. For this
-         * sample, the position is 2 so the expected true run number is
-         * one less than the "run number" in the 2G file.
+         * sample, the position is 2 so the expected true run number is one less
+         * than the "run number" in the 2G file.
          */
         assertEquals(expected[3] - 1, step.getRunNumber(), 0);
-        
+
         assertEquals(new TreatmentType[]
             {TreatmentType.NONE, TreatmentType.DEGAUSS_XYZ}[(int) expected[4]],
                 step.getTreatmentType());
-        
+
         /*
          * Divide values in file by 10000 to convert from oersted to tesla.
          */
@@ -156,5 +153,65 @@ public class TwoGeeLoaderTest {
         assertEquals(expected[5] / 10000, step.getAfY(), DELTA);
         assertEquals(expected[5] / 10000, step.getAfZ(), DELTA);
     }
+    
+    private static final double[][] expected_fq0101_1 = {
+        {20, 2.4097E-05, 9.4826E-06, 5.8278E-06, 6.0466},
+        {40, 0.00011177, 1.1972E-05, -4.4373E-05, 7.0455},
+        {64, 6.9455E-05, 4.4971E-06, -4.0854E-06, 7.0000},
+        {102, -1.9346E-06, 2.3703E-06, -9.1195E-06, 7.0000},
+        {123, -1.8121E-05, -8.0666E-06, 8.4264E-06, 7.0454}
+    };
+    
+    @Test
+    public void testThermal1posMagSus() throws IOException {
+        final Path filePath = copyFile("FQ0101.1.DAT");
+        final TwoGeeLoader loader = new TwoGeeLoader(filePath.toFile(),
+                TwoGeeLoader.Protocol.NORMAL, new Vec3(4.628, -4.404, -6.280),
+                false);
+        for (int i = 0; i < loader.getTreatmentSteps().size(); i++) {
+            final double[] expected = expected_fq0101_1[i];
+            final TreatmentStep step = loader.getTreatmentSteps().get(i);
+            checkVariableFieldsFq0101_1(expected, step);
+            checkConstantFieldsFq0101_1(step);
+        }        
+    }
+
+    private void checkVariableFieldsFq0101_1(double[] expected,
+            TreatmentStep step) {
+        assertEquals(expected[0], step.getTemperature(), DELTA);
+        assertEquals(expected[1], step.getMoment().x, DELTA);
+        assertEquals(expected[2], step.getMoment().y, DELTA);
+        assertEquals(expected[3], step.getMoment().z, DELTA);
+        assertEquals(expected[4], step.getMagSus(), DELTA);
+    }
+    
+    private void checkConstantFieldsFq0101_1(TreatmentStep step) {
+        assertEquals(MeasurementType.DISCRETE, step.getMeasurementType());
+        assertEquals(10, step.getVolume(), DELTA);
+        assertEquals(4, step.getArea(), DELTA);
+        assertEquals(132, step.getSampAz(), DELTA);
+        assertEquals(19, step.getSampDip(), DELTA);
+        assertEquals(0, step.getFormAz(), DELTA);
+        assertEquals(0, step.getFormDip(), DELTA);
+        assertEquals(0, step.getMagDev(), DELTA);
+        assertEquals(TreatmentType.THERMAL, step.getTreatmentType());
+        assertEquals(0, step.getAfX(), DELTA);
+        assertEquals(0, step.getAfY(), DELTA);
+        assertEquals(0, step.getAfZ(), DELTA);
+        assertTrue(Double.isNaN(step.getIrmField()));
+        assertTrue(Double.isNaN(step.getArmField()));
+        assertEquals(ArmAxis.NONE, step.getArmAxis());
+    }
+    
+    private Path copyFile(String filename) throws IOException {
+        final Path filePath
+                = temporaryFolder.getRoot().toPath().resolve(filename);
+        Files.copy(
+                TestFileLocator.class.getResourceAsStream("twogee/" + filename),
+                filePath);
+        return filePath;
+    }
+    
+
     
 }
