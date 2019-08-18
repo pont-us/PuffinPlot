1ac8fb688ac6bdca0ca02b683b1619a8fd5c056a
Pontus Lurcock <pont@talvi.net>
2019-08-18 14:14:21 +0100

    Improve the layout of the About box
    Mainly adding some blank space around the components. No changes in
    content or functionality, except for duplicating the version number and
    date in the main text field as well as in its own JLabel; this makes it
    possible to copy-paste, which might be useful for bug reporters.

--- src/net/talvi/puffinplot/window/AboutBox.java
+++ src/net/talvi/puffinplot/window/AboutBox.java
@@ -18,15 +18,20 @@
 package net.talvi.puffinplot.window;
 
 import java.awt.Dimension;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 import net.talvi.puffinplot.PuffinApp;
+
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
 
 /**
  * An ‘about this program’ dialog box giving brief information
@@ -43,56 +48,49 @@ public class AboutBox extends JDialog {
      * @param app the PuffinPlot instance associated with this dialog
      */
     public AboutBox(PuffinApp app) {
-        super(app.getMainWindow(), "About PuffinPlot", true);
+        super(app.getMainWindow(), "About PuffinPlot",
+                ModalityType.APPLICATION_MODAL);
 
-        setMinimumSize(new Dimension(700, 400));
-        setModal(true);
-        getContentPane().setLayout(
-                new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        setPreferredSize(new Dimension(600, 360));
+        final BoxLayout layout =
+                new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
+        getContentPane().setLayout(layout);
 
+        getContentPane().add(Box.createVerticalStrut(8));
         final JLabel heading = new JLabel("PuffinPlot", SwingConstants.CENTER);
         heading.setFont(heading.getFont()
                 .deriveFont(heading.getFont().getSize() + 8F));
         heading.setAlignmentX(0.5F);
-        heading.setMaximumSize(new Dimension(5000, 25));
-        heading.setPreferredSize(null);
         getContentPane().add(heading);
 
-        final JTextArea textArea = new JTextArea(getMainText(app), 5, 20);
+        final JTextArea textArea = new JTextArea(getMainText(app));
         textArea.setEditable(false);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
-        textArea.setPreferredSize(null);
+        textArea.setMargin(new Insets(8, 8, 8, 8));
 
-        final JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setHorizontalScrollBarPolicy(
-                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setViewportView(textArea);
+        final JScrollPane scrollPane = new JScrollPane(textArea,
+                VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
         getContentPane().add(scrollPane);
 
         final JLabel versionInfo = new JLabel(
-                String.format("Version: %s. Date: %s.",
-                        app.getVersion().getVersionString(),
-                        app.getVersion().getDateString()),
-                SwingConstants.CENTER);
+                getVersionString(app), SwingConstants.CENTER);
         versionInfo.setAlignmentX(0.5F);
-        versionInfo.setMaximumSize(new Dimension(500, 30));
-        versionInfo.setMinimumSize(new Dimension(45, 30));
-        versionInfo.setPreferredSize(new Dimension(500, 30));
         getContentPane().add(versionInfo);
+        getContentPane().add(Box.createVerticalStrut(8));
 
         final JButton closeButton = new JButton("Close");
         closeButton.setAlignmentX(0.5F);
-        closeButton.setMaximumSize(new Dimension(120, 29));
-        closeButton.setPreferredSize(new Dimension(500, 29));
+        closeButton.setPreferredSize(new Dimension(120, 30));
         closeButton.addActionListener(e -> setVisible(false));
         getContentPane().add(closeButton);
+        getContentPane().add(Box.createVerticalStrut(8));
 
         pack();
     }
 
-
-    private String getMainText(PuffinApp app) {
+    private static String getMainText(PuffinApp app) {
         final String fmt =
                 "PuffinPlot is a program for showing, exploring, and analysing "
                 + "palæomagnetic data. Copyright %s Pontus "
@@ -106,15 +104,20 @@ public class AboutBox extends JDialog {
                 + "of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. "
                 + "Please see the file LICENCE (supplied with PuffinPlot) "
                 + "or go to http://www.gnu.org/licenses/gpl.html "
-                + "for details.\n\n"
+                + "for details.\n\n%s\n"
                 + "System information: OS %s %s (%s), Java %s (%s)";
         return String.format(fmt, app.getVersion().getYearRange(),
-                gsp("os.name"), gsp("os.version"),
+                getVersionString(app), gsp("os.name"), gsp("os.version"),
                 gsp("os.arch"), gsp("java.version"), gsp("java.vendor"));
     }
-
-    private String gsp(String property) {
-        return System.getProperty(property);
+    
+    private static String getVersionString(PuffinApp app) {
+        return String.format("Version: %s. Date: %s.",
+                app.getVersion().getVersionString(),
+                app.getVersion().getDateString());
     }
 
+    private static String gsp(String property) {
+        return System.getProperty(property);
+    }
 }
